bc398410a051eb3e3bfccd3eb59f7f7f5b3649cb
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2019-05-14 20:07:01 +0300

    ~ fix

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/IrElementToJsExpressionTransformer.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/IrElementToJsExpressionTransformer.kt
@@ -5,6 +5,7 @@
 
 package org.jetbrains.kotlin.ir.backend.js.transformers.irToJs
 
+import org.jetbrains.kotlin.backend.common.ir.isElseBranch
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.ir.backend.js.utils.*
 import org.jetbrains.kotlin.ir.declarations.IrClass
@@ -309,7 +310,12 @@ class IrElementToJsExpressionTransformer : BaseIrElementToJsNodeTransformer<JsEx
     }
 
     override fun visitWhen(expression: IrWhen, context: JsGenerationContext): JsExpression {
-        val implicitElse = JsPrefixOperation(JsUnaryOperator.VOID, JsIntLiteral(0))
+        val implicitElse =
+            if (expression.branches.find(::isElseBranch) == null)
+                JsPrefixOperation(JsUnaryOperator.VOID, JsIntLiteral(0))
+            else
+                null
+
         return expression.toJsNode(this, context, ::JsConditional, implicitElse)!!
     }
 
