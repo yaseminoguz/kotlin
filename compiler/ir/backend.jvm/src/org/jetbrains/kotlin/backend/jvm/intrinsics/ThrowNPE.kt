413b3e44731f3a88ad20c1c1939b3dedb48ca7e4
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2019-07-26 17:05:47 +0200

    Use Intrinsics.checkNotNull and throw NPE in !! operator generation
    This method was introduced in c204e8fc67 "just in case" and was never
    used. Therefore we're free to change its semantics and use it in all new
    generated code (with API version >= 1.4), without even the
    isApiVersionAtLeast check in inline functions. Since we agreed to change
    the type of thrown exceptions to java.lang.NPE in KT-22275, call a new
    method throwJavaNpe now which throws that exception instead of KNPE.
    
    Note that the additional method that takes an exception message is still
    unused and exists just in case we need to use it in the future.
    
     #KT-22275 In Progress

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/intrinsics/ThrowNPE.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/intrinsics/ThrowNPE.kt
@@ -8,7 +8,6 @@ package org.jetbrains.kotlin.backend.jvm.intrinsics
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.codegen.intrinsics.IntrinsicMethods
 import org.jetbrains.kotlin.ir.expressions.IrFunctionAccessExpression
-import org.jetbrains.kotlin.ir.expressions.IrMemberAccessExpression
 import org.jetbrains.kotlin.resolve.jvm.jvmSignature.JvmMethodSignature
 import org.jetbrains.org.objectweb.asm.Type
 
@@ -19,6 +18,10 @@ object ThrowNPE : IntrinsicMethod() {
         context: JvmBackendContext
     ): IrIntrinsicFunction {
         return IrIntrinsicFunction.createWithResult(expression, signature, context) {
+            // Note that to support the new behavior of null checks in 1.4 (KT-22275) we could simply change this to "throwJavaNpe".
+            // However, it would be preferable to refactor the null check generation in the IR backend in a way that would allow us
+            // to call "Intrinsics.checkNotNull" instead, as in the old backend. That way, the bytecode will be shorter and we'll
+            // avoid dependency on another method from stdlib (which will allow us to refactor it if needed in the future).
             it.invokestatic(IntrinsicMethods.INTRINSICS_CLASS_NAME, "throwNpe", "()V", false)
             Type.VOID_TYPE
         }
---
+++ compiler/testData/codegen/bytecodeText/nullCheckOptimization/multipleExclExcl_1_4.kt
@@ -0,0 +1,12 @@
+// !API_VERSION: 1.4
+
+fun test(s: String?): Int {
+    s!!
+    s!!
+    s!!
+    s!!
+    s!!
+    return 0
+}
+
+// 1 checkNotNull
